c94fdda46470179b6a34f05ecc4b5917ef1fa483
Oluwatola Ayomide <ayomideoluwatola1@gmail.com>
2026-04-20 12:31:53 +0100

    exercise 7-10

--- topics/tuple/exercises/src/lib.rs
+++ topics/tuple/exercises/src/lib.rs
@@ -1,11 +1,14 @@
 pub fn first(t: (bool, u32, char)) -> bool {
-    todo!();
+    let (a, _, _) = t;
+    a
 }
 
 pub fn last(t: (bool, u32, char)) -> char {
-    todo!();
+    let (_, _, c) = t;
+    c
 }
 
 pub fn swap(t: (u32, u32)) -> (u32, u32) {
-    todo!();
+    let (a, b) = t;
+    (b, a)
 }
