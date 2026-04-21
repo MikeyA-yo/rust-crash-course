6c9ece1d09c291696bf755cdc217710b7666896e
Oluwatola Ayomide <ayomideoluwatola1@gmail.com>
2026-04-21 15:05:25 +0100

    exercise 26-27

--- topics/for_loop/exercises/src/lib.rs
+++ topics/for_loop/exercises/src/lib.rs
@@ -1,7 +1,15 @@
 pub fn sum(nums: Vec<i32>) -> i32 {
-    todo!();
+    let mut sum = 0;
+    for n in nums.iter()  {
+        sum += n;
+    }
+    sum
 }
 
 pub fn fill(i: u32, n: usize) -> Vec<u32> {
-    todo!();
+    let mut result = Vec::new();
+    for _ in 0..n {
+        result.push(i);
+    }
+    result
 }
