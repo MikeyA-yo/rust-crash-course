ec69c7eadafdb91d4ba146043ea4fd735e377750
Oluwatola Ayomide <ayomideoluwatola1@gmail.com>
2026-04-23 08:50:09 +0100

    more excercise

--- topics/pattern_match/exercises/src/lib.rs
+++ topics/pattern_match/exercises/src/lib.rs
@@ -1,7 +1,18 @@
 pub fn num_to_string(num: u32) -> String {
-    todo!();
+    let x = match num {
+        0 => "zero".to_string(),
+        1 => "one".to_string(),
+        2 => "two".to_string(),
+        3 => "three".to_string(),
+        _ => "other".to_string(),
+    };
+    x
 }
 
 pub fn unwrap_or_default(x: Option<u32>, v: u32) -> u32 {
-    todo!();
+    let x = match x {
+        Some(x) => x,
+        None => v,
+    };
+    x
 }
