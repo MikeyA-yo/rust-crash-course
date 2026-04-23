66c23059d1946fce7de7065f20da94a358eed3bc
Oluwatola Ayomide <ayomideoluwatola1@gmail.com>
2026-04-23 16:00:26 +0100

    more excercise..

--- topics/borrowing_rules/exercises/src/main.rs
+++ topics/borrowing_rules/exercises/src/main.rs
@@ -1,28 +1,27 @@
 #![allow(unused)]
 
-fn print_len(s: String) {
+fn print_len(s: &String) {
     println!("len: {}", s.len());
 }
 
 fn main() {
     // Exercise 1
     let s = String::from("Rust");
-    let s1 = &mut s;
-    let s2 = &mut s;
-
+    let s1 = &s;
     println!("s1: {s1}");
+    let s2 = &s;
     println!("s2: {s2}");
 
     // Exercise 2
     let mut s = String::from("Rust");
     let s1 = &mut s;
-    let s2 = &mut s;
+    // let s2 = &mut s;
 
     s1.push_str("!!!");
     println!("s: {s}");
 
     // Exercise 3
     let s = String::from("Rust");
-    print_len(s);
+    print_len(&s);
     println!("s: {s}");
 }
