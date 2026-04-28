896b1152a5c8e6415d27937723b172cb4f874460
Oluwatola Ayomide <ayomideoluwatola1@gmail.com>
2026-04-28 15:34:24 +0100

    add async await

--- topics/async_await/exercises/src/main.rs
+++ topics/async_await/exercises/src/main.rs
@@ -1,18 +1,18 @@
 use std::time::Duration;
 use tokio::time::sleep;
 
-fn hello() {
+async fn hello() {
     println!("Hello async Rust!");
 }
 
-fn add(x: u32, y: u32) -> u32 {
-    sleep(Duration::from_millis(1000));
+async fn add(x: u32, y: u32) -> u32 {
+    sleep(Duration::from_millis(1000)).await;
     x + y
 }
+#[tokio::main]
+async fn main() {
+    hello().await;
 
-fn main() {
-    hello();
-
-    let sum = add(1, 2);
+    let sum = add(1, 2).await;
     println!("sum: {sum}");
 }
